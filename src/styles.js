359ed9e0dde8d7a47d10d8363946543cfd22477b
ronitli <ronitli@mta.ac.il>
2023-04-28 02:43:02 +0300

    initial design

--- src/styles.js
+++ src/styles.js
@@ -1,18 +1,29 @@
 import { StyleSheet } from 'react-native';
 
+
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#FCEFEF',
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    color: '#A0522D',
+    marginBottom: 50,
+   // fontFamily: 'Nunito-Bold',
   },
   input: {
-    height: 40,
-    width: '90%',
-    borderColor: 'gray',
+    width: '80%',
+    height: 50,
+    padding: 10,
     borderWidth: 1,
-    margin: 10,
-    paddingLeft: 10,
+    borderColor: 'gray',
+    marginBottom: 20,
+    backgroundColor: 'white',
+    borderRadius: 10,
   },
   buttonContainer: {
     flexDirection: 'row',
@@ -37,5 +48,29 @@ export const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
     marginBottom: 10,
+    color: '#6F4E37',
+  },
+  loginButton: {
+    backgroundColor: '#E59C8A',
+    width: '80%',
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+  registerButton: {
+    backgroundColor: '#B08975',
+    width: '80%',
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    fontWeight: 'bold',
   },
 });
