666c2dce85faa71eb40be2ddbeb8f177f4a246e3
ronitli <ronitli@mta.ac.il>
2023-05-15 21:57:05 +0300

    home page is done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 // Home.js
 
 import React from "react";
-import { View, Text, TouchableOpacity,StyleSheet  } from "react-native";
+import { View, Text, TouchableOpacity,StyleSheet, Image  } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { useNavigation } from "@react-navigation/native";
@@ -12,30 +12,46 @@ import { List, MD3Colors,Divider ,Surface} from 'react-native-paper';
 const Home = ({ navigation }) => {
   return (
     <View style={styles.container}> 
+   
     <Icon name="home" size={60} color="#A0522D" />
     <Text style={styles.title}>Home Page</Text>
     
        <View style={styles.topRow}>
         <TouchableOpacity onPress={() => navigation.navigate('Requests')}>
-        <Surface style={styles.surface} ><Text>Manage Requests</Text></Surface>
+        <Surface style={styles.surface} >
+        <Icon name="clipboard" size={25} color='sienna' />
+          <Text style={styles.radioButtonText}>Manage Requests</Text></Surface>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => navigation.navigate('AddDog')}>
-        <Surface style={styles.surface}><Text>Add a New Dog</Text></Surface>
+        <Surface style={styles.surface}>
+        <Icon name="plus" size={25} color='sienna' />
+        <View style={{ height: 10 }} />
+          <Text style={styles.radioButtonText}>Add a New Dog</Text></Surface>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => navigation.navigate('Dogs')}>
-        <Surface style={styles.surface}><Text>All Dogs</Text></Surface>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.bottomRow}>
-       <TouchableOpacity onPress={() => navigation.navigate('Calender')}>
-        <Surface style={styles.surface}><Text>Calendar</Text></Surface>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate('Alerts')}>
-        <Surface style={styles.surface}><Text>Check Notifications</Text></Surface>
+        <Surface style={styles.surface}>
+        <Icon name="paw" size={25} color='sienna' />
+        <View style={{ height: 10 }} />
+          <Text style={styles.radioButtonText}>All Dogs</Text></Surface>
         </TouchableOpacity>
       </View>
      
-
+      <View style={styles.bottomRow}>
+       <TouchableOpacity onPress={() => navigation.navigate('Calender')}>
+        <Surface style={styles.downsurface}>
+        <Icon name="calendar" size={25} color='sienna' />
+        <View style={{ height: 10 }} />
+          <Text style={styles.radioButtonText}>Calendar</Text></Surface>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate('Alerts')}>
+        <Surface style={styles.downsurface}>
+        <Icon name="bell" size={25} color='sienna' />
+        <View style={{ height: 5 }} />
+          <Text style={styles.radioButtonText}>     Check Notifications</Text></Surface>
+        </TouchableOpacity>
+      </View>
+    
+      <Image source={require('../../assets/try.jpg')} style={{ width: 410, height: 320  }}/>
       
     </View>
   );
