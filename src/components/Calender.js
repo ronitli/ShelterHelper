3bc275c6ed51dab24708a27eec2a8cba0ce64669
ronitli <ronitli@mta.ac.il>
2023-07-30 23:35:22 +0300

    Added calendar events

--- src/components/Calender.js
+++ src/components/Calender.js
@@ -1,5 +1,5 @@
 import React,{ useState } from 'react';
-import { View, Text,  StyleSheet,Dimensions } from 'react-native';
+import { View, Text,  StyleSheet,Dimensions ,TouchableOpacity, TextInput,FlatList, ScrollView,Pressable } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useNavigation } from '@react-navigation/native';
@@ -7,18 +7,101 @@ import Drawer from 'react-native-drawer';
 import { Calendar,CalendarList, Agenda } from 'react-native-calendars';
 import { styles } from '../styles';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import EventModal from './EventModal';
+import AllEventsModal from './AllEventsModal';
+import { Alert } from 'react-native';
 
 const Calender = ({navigation}) => {
   const [selectedDate, setSelectedDate] = useState('');
   const [events, setEvents] = useState({});
-  const addEvent = (day) => {
-    const newEvents = { ...events };
-    newEvents[day.dateString] = { selected: true, marked: true, dotColor: 'blue' };
-    setEvents(newEvents);
-  };
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isAllEventsModalVisible, setIsAllEventsModalVisible] = useState(false);
+
+  
   const onDayPress = (day) => {
     setSelectedDate(day.dateString);
   };
+
+  const handleDeleteEvent = (date, event) => {
+    const selectedDateEvents = events[date];
+  
+    // Create a copy of the events array and remove the event from it
+    const updatedEvents = selectedDateEvents.filter((item) => item !== event);
+  
+    // Update the events state with the modified array
+    const updatedEventsState = {
+      ...events,
+      [date]: updatedEvents,
+    };
+  
+    // Remove the date entry from the state if all events for that date are deleted
+    if (updatedEvents.length === 0) {
+      delete updatedEventsState[date];
+    }
+  
+    // Set the updated events state
+    setEvents(updatedEventsState);
+  
+    // Close the AllEventsModal after deleting
+    //setIsAllEventsModalVisible(false);
+  };
+  
+
+  // const handleDeleteEvent = (date, index) => {
+  // const selectedDateEvents = events[date];
+  // const updatedEvents = selectedDateEvents.filter((event, i) => i !== index);
+  // const updatedEventsState = {
+  //   ...events,
+  //   [date]: updatedEvents,
+  // };
+  // if (updatedEvents.length === 0) {
+  //   delete updatedEventsState[date];
+  // }
+  // setEvents(updatedEventsState);
+  // setIsModalVisible(false);
+  // };
+
+const handleCreateEvent = () => {
+  // Show the event modal when the user clicks "Create Event"
+  setIsModalVisible(true);
+};
+
+const handleSaveEvent = (newEvent) => {
+  
+  // Save the new event in the events state using the selected date as the key
+  if (events[selectedDate]) {
+    // If events exist, add the new event to the existing array of events for the selected date
+    const newEvents = [...events[selectedDate], newEvent];
+    setEvents({ ...events, [selectedDate]: newEvents });
+  } else {
+    // If events don't exist, create a new array with the new event as the first element
+    setEvents({ ...events, [selectedDate]: [newEvent] });
+  }
+ 
+//save to database
+alert("Event created!");
+};
+const handleOpenAllEventsModal = () => {
+  setIsAllEventsModalVisible(true);
+};
+const handleCloseAllEventsModal = () => {
+  setIsAllEventsModalVisible(false);
+};
+
+const handleCloseModal = () => {
+  // Close the modal
+  setIsModalVisible(false);
+};
+
+const markedDates = {};
+
+for (const date in events) {
+  if (events.hasOwnProperty(date)) {
+    markedDates[date] = { marked: true, dotColor: 'sienna'};
+  }
+  
+}
+
 
     return (
       <View style={styles.container}>
@@ -31,6 +114,7 @@ const Calender = ({navigation}) => {
        
         onDayPress={onDayPress}
         markedDates={{
+          ...markedDates,
           [selectedDate]: {
             selected: true,
             disableTouchEvent: true,
@@ -50,12 +134,81 @@ const Calender = ({navigation}) => {
         }}
       />
       </View>
+      
+
+      {/* Display created events */}
+      {isModalVisible ? null : (
+      events[selectedDate] && (
+        <View style={styles.eventListContainer}>
+          
+        {events[selectedDate].slice(0, 4).map((event, index) => (
+          <View key={index} style={styles.eventDetailsContainer}>
+            <Text style={styles.notificationTitle}>&#x00B7; {event.name}</Text>
+          </View>
+        ))}
+          </View >
+      )
+      )}
+
+<View style={{ height: 10 }} />
+{/* {selectedDate && (
+  <View style={styles.createEventContainer}>
+    
+      <TouchableOpacity style={styles.loginButton} onPress={handleCreateEvent}>
+        <Text style={styles.buttonText}>Add Event </Text>
+      </TouchableOpacity>
+   
+
+    {isModalVisible && (
+      <EventModal isVisible={isModalVisible} onClose={handleCloseModal} onSave={handleSaveEvent} />
+    )}
+  </View>
+)} */}
+{!isModalVisible && selectedDate &&(
+  
+<View style={styles.buttonContainer}>
+<Pressable onPress={handleOpenAllEventsModal}>
+        <Text style={styles.notificationTitle}> touch here to show ALL events for this date</Text>
+      </Pressable>
+</View>
+)}
+<View style={{ height: 10 }} />
+{selectedDate && (
+  <View style={styles.createEventContainer}>
+    
+      <TouchableOpacity style={styles.loginButton} onPress={handleCreateEvent}>
+        <Text style={styles.buttonText}>Add Event </Text>
+      </TouchableOpacity>
+   
+
+    {isModalVisible && (
+      <EventModal isVisible={isModalVisible} onClose={handleCloseModal} onSave={handleSaveEvent} />
+    )}
+  </View>
+)}
+ <AllEventsModal
+        isVisible={isAllEventsModalVisible}
+        onClose={handleCloseAllEventsModal}
+        events={events[selectedDate]}
+        selectedDate={selectedDate} 
+        onDelete={handleDeleteEvent}
+      />
     </View>
+
       );
   
 };
 const { width, height } = Dimensions.get('window');
 const style = StyleSheet.create({
+  eventListContainer: {
+    flex: 1,
+    flexDirection: 'column', // Align events in a vertical list
+   justifyContent: 'center',
+    marginTop: 'auto',
+    alignItems: 'center',
+    marginVertical: 10,
+  // height: 20, // Align events to the top of the container
+  },
   container: {
     flex: 1,
     width: width,
---
+++ src/components/EventModal.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+
+const EventModal = ({ isVisible, onClose, onSave }) => {
+  const [eventName, setEventName] = useState('');
+ 
+  const handleSaveEvent = () => {
+    // Create a new event object with the provided details
+     const newEvent = {
+      name: eventName,
+     };
+     const trimmedEventName = eventName.trim();
+     if(trimmedEventName === '')
+     {
+        alert("You can not create an empty event!");
+        return;
+     }
+    // Call the onSave function to save the new event
+    onSave(newEvent);
+    // Clear the input fields
+    setEventName('');
+   
+    // Close the modal
+    onClose();
+  };
+
+  return (
+    <View style={styles.modalContainer}>
+      {/* <Text style={styles.modalTitle}>Add Event</Text> */}
+      <TextInput
+        style={styles.input}
+        placeholder="Event:"
+        placeholderTextColor="#D6A6A6"
+        value={eventName}
+        onChangeText={setEventName}
+      />
+     
+      <TouchableOpacity style={styles.saveButton} onPress={handleSaveEvent}>
+        <Text style={styles.saveButtonText}>Save Event</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.cancelButton} onPress={onClose}>
+        <Text style={styles.cancelButtonText}>Cancel</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 5,
+  },
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#D6A6A6',
+    
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  },
+  saveButton: {
+    backgroundColor: 'sienna',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  saveButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  cancelButton: {
+    padding: 10,
+    alignItems: 'center',
+  },
+  cancelButtonText: {
+    color: 'sienna',
+    fontWeight: 'bold',
+  },
+});
+
+export default EventModal;
