a9b84e78e9c3147f1c72f5e9ba93d86553745719
lirzo <lirzomer497@gmail.com>
2023-08-24 11:26:11 +0300

    changeslogin added tests

---
+++ src/components/DoneAlerts.js
@@ -0,0 +1,62 @@
+// import React,{ useState,useEffect } from 'react';
+// import {
+//   SafeAreaView,
+//   View,
+//   FlatList,
+//   StyleSheet,
+//   Text,
+//   StatusBar,
+//   ScrollView,
+//   Pressable,
+//   TouchableOpacity,
+//   Modal,
+//   TextInput,
+//   Alert
+// } from 'react-native';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+// import { useNavigation } from '@react-navigation/native';
+// import Icon from 'react-native-vector-icons/FontAwesome';
+// import { styles } from '../styles';
+// import {
+//   collection,
+//   doc,
+//   getDoc,
+//   setDoc,
+//   addDoc,
+//   updateDoc,
+//   deleteDoc,
+//   getDocs,
+// } from "firebase/firestore";
+// import { db, storage } from "../../firebase";
+
+// const DoneAlerts = ({ navigation }) => {
+//   const [alerts, setAlerts] = useState([]);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       const q = collection(db, "ComletedNotifications");
+//       const querySnapshot = await getDocs(q);
+//       let alertsArray = querySnapshot.docs.map((doc) => doc.data());
+
+//       setAlerts(alertsArray);
+//     };
+//     fetchData();
+//   }, [alerts]);
+
+//   return (
+//     <View style={styles.container}>
+//         <View style={{   marginTop: 200 }} />
+//      <Icon name="check" size={50} color='sienna' />
+//    <Text style={styles.title}>  Completed Notifications</Text>
+
+//    <SafeAreaView >
+//    <ScrollView style={styles.scrollContainer}>
+//     </ScrollView>
+//     </SafeAreaView>
+
+//     </View>
+//   );
+// };
+
+// export default DoneAlerts;
